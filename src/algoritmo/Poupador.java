7405b915cb4958a0ce7e7adecfa27d956e085594
Matheus Timbó <matheusfild4@hotmail.com>
2020-04-05 18:04:18 -0300

    Refactoring project

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -1,131 +1,173 @@
 package algoritmo;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 public class Poupador extends ProgramaPoupador {
 
-	int cont = 0;
-	int[][] memoriaExploracao = new int[30][30];
+	PossibilidadeMovimento acima, esquerda, direita, baixo;
 
+	public class PossibilidadeMovimento {
+		boolean isViavel;
+		int x;
+		int y;
+		Visao visao;
+		int coordenadaVisao;
+		int comandoMovimento;
+		public PossibilidadeMovimento(boolean isViavel, int x, int y, Visao visao, int coordenadaVisao, int comandoMovimento){
+			this.isViavel = isViavel;
+			this.x = x;
+			this.y = y;
+			this.visao = visao;
+			this.coordenadaVisao = coordenadaVisao;
+			this.comandoMovimento = comandoMovimento;
+		}
+	}
+
+	void mapearMovimentos(){
+		int x = sensor.getPosicao().x;
+		int y = sensor.getPosicao().y;
+		System.out.println("x: " + x + " y: " + y);
+		acima = new PossibilidadeMovimento(movimentoIsViavel(visaoacima), x, y-1, getResultVisao(sensor.getVisaoIdentificacao()[visaoacima]), visaoacima, movercima);
+		esquerda = new PossibilidadeMovimento(movimentoIsViavel(visaoesquerda), x-1, y, getResultVisao(sensor.getVisaoIdentificacao()[visaoesquerda]), visaoesquerda, moveresquerda);
+		direita = new PossibilidadeMovimento(movimentoIsViavel(visaodireita), x+1, y, getResultVisao(sensor.getVisaoIdentificacao()[visaodireita]), visaodireita, moverdireita);
+		baixo = new PossibilidadeMovimento(movimentoIsViavel(visaobaixo), x, y+1, getResultVisao(sensor.getVisaoIdentificacao()[visaobaixo]), visaobaixo, moverbaixo);
+	}
+
+	public Objetivo objetivo;
+
+	public enum Objetivo {
+		FUGIR,
+		EXPLORAR,
+		GUARDAR_MOEDAS,
+	}
+
+	public enum Visao {
+		SEMVISAO(-2),
+		FORA(-1),
+		VAZIO(0),
+		PAREDE(1),
+		BANCO(3),
+		MOEDA(4),
+		PASTINHA(5),
+		POUPADOR(100),
+		LADRAO(200);
+		private final int valor;
+		Visao(int i){
+			valor = i;
+			}
+			public int get(){
+				return valor;
+			}
+	}
+
+	public Visao getResultVisao(int i){
+		switch(i){
+			case -2:
+				return Visao.SEMVISAO;
+			case -1:
+				return Visao.FORA;
+			case 0:
+				return Visao.VAZIO;
+			case 1:
+				return Visao.PAREDE;
+			case 3:
+				return Visao.BANCO;
+			case 4:
+				return Visao.MOEDA;
+			case 5:
+				return Visao.PASTINHA;
+			case 100:
+				return Visao.POUPADOR;
+			case 200:
+				return Visao.LADRAO;
+			default:
+				return Visao.VAZIO;
+		}
+	}
+
+	int visaoacima = 7;
+	int visaoesquerda = 11;
+	int visaodireita = 12;
+	int visaobaixo = 16;
+
+	int ficarparado = 0;
+	int movercima = 1;
+	int moverbaixo = 2;
+	int moverdireita = 3;
+	int moveresquerda = 4;
+
+	int[][] memoria = new int[31][31];
+	
 	public int acao() {
 		int x = sensor.getPosicao().x;
 		int y = sensor.getPosicao().y;
-		memoriaExploracao[x][y]++;
-		return explorar(x, y);
+		memoria[x][y]++;
+		mapearMovimentos();
+		this.objetivo = decidirObjetivo();
+
+
+		return decidirAcaoProObjetivo(Objetivo.EXPLORAR);
 	}
 
-	//0 parado
-	//1 cima
-	//2 baixo
-	//3 direita
-	//4 esquerda
-	// y -1 subir
-	// x -1 esquerda
-	// y +1 descer
-	// x +1 direita
+	int decidirAcaoProObjetivo(Objetivo objetivo){
+		if(objetivo == Objetivo.EXPLORAR){
+			return getLugarMenosExplorado();
+		} else {
+			return 0;
+		}
+	}
 
-	int explorar(int x, int y){
-
+	int getLugarMenosExplorado(){
 		int menorPeso = Integer.MAX_VALUE;
-		int direcaoMenorPeso = 0;
-		Map pesosCaminhos = new HashMap();
-		for(int i =1; i< 5; i++){ //1 cima 2 baixo 3 direita 4esquerda
-			int indiceVisao = -2;
-			int xDirecao = x;
-			int yDirecao = y;
-			if(i == 1) {
-				indiceVisao = 7;
-				yDirecao--;
-				if (isCaminhoViavel(sensor.getVisaoIdentificacao()[indiceVisao])) {
-					int pesoCima = getPesoMemoria(xDirecao, yDirecao);
-					System.out.println("Peso cima: " + pesoCima);
-					pesosCaminhos.put(pesoCima, 1);
-				}
+		if(acima.isViavel && memoria[acima.x][acima.y] < menorPeso){
+			menorPeso = memoria[acima.x][acima.y];
+		}
+		if(esquerda.isViavel && memoria[esquerda.x][esquerda.y] < menorPeso){
+			menorPeso = memoria[esquerda.x][esquerda.y];
+		}
+		if(direita.isViavel && memoria[direita.x][direita.y] < menorPeso){
+			menorPeso = memoria[direita.x][direita.y];
+		}
+		if(baixo.isViavel && memoria[baixo.x][baixo.y] < menorPeso){
+			menorPeso = memoria[baixo.x][baixo.y];
+		}
+		ArrayList<PossibilidadeMovimento> opcoesIguais = new ArrayList<>();
+		if(acima.isViavel && memoria[acima.x][acima.y] == menorPeso){
+			opcoesIguais.add(acima);
+		}
+		if(esquerda.isViavel && memoria[esquerda.x][esquerda.y] == menorPeso){
+			opcoesIguais.add(esquerda);
+		}
+		if(direita.isViavel && memoria[direita.x][direita.y] == menorPeso){
+			opcoesIguais.add(direita);
+		}
+		if(baixo.isViavel && memoria[baixo.x][baixo.y] == menorPeso){
+			opcoesIguais.add(baixo);
+		}
+		Random random = new Random();
+		if(opcoesIguais.size() == 0){
+			return 0;
+		} else {
+			int indexrandom = random.nextInt(opcoesIguais.size());
+			System.out.println("visoes das opcoes iguais");
+			for(int i =0 ; i < opcoesIguais.size(); i++){
+				System.out.println(opcoesIguais.get(i).visao);
 			}
-			if(i == 2) {
-				indiceVisao = 16;
-				yDirecao++;
-				if (isCaminhoViavel(sensor.getVisaoIdentificacao()[indiceVisao])) {
-					int pesoBaixo = getPesoMemoria(xDirecao, yDirecao);
-					System.out.println("Peso baixo: " + pesoBaixo);
-					pesosCaminhos.put(pesoBaixo, 2);
-				}
-			}
-			if(i == 3){
-				indiceVisao = 12;
-				xDirecao++;
-				if (isCaminhoViavel(sensor.getVisaoIdentificacao()[indiceVisao])) {
-					int pesoDireita = getPesoMemoria(xDirecao, yDirecao);
-					System.out.println("Peso direita: " + pesoDireita);
-					pesosCaminhos.put(pesoDireita, 3);
-				}
-			}
-			if( i == 4) {
-				indiceVisao = 11;
-				xDirecao--;
-				if (isCaminhoViavel(sensor.getVisaoIdentificacao()[indiceVisao])) {
-					int pesoEsquerda = getPesoMemoria(xDirecao, yDirecao);
-					System.out.println("Peso esquerda: " + pesoEsquerda);
-					pesosCaminhos.put(pesoEsquerda, 4);
-				}
-			}
-
-			if (sensor.getVisaoIdentificacao()[indiceVisao] == 1) {
-				memoriaExploracao[x][y] = Integer.MAX_VALUE;
-			}else if(isCaminhoViavel(sensor.getVisaoIdentificacao()[indiceVisao])){
-				if(getPesoMemoria(xDirecao, yDirecao) < menorPeso){
-					System.out.println("peso memoria ["+xDirecao+"]["+yDirecao+"] = " + getPesoMemoria(xDirecao, yDirecao));
-					menorPeso = getPesoMemoria(x, y);
-					direcaoMenorPeso = i;
-				}
-			}
+			return opcoesIguais.get(indexrandom).comandoMovimento;
 		}
 
-		for (int j = 0; j < 30; j++) {
-			for (int i = 0; i < 30; i++) {
-				cont++;
-				if (memoriaExploracao[i][j] < Integer.MAX_VALUE) {
-					if((i==x) && (j == y)){
-						System.out.print("@ ");
-					}else{
-						System.out.print(memoriaExploracao[i][j] + " ");
-					}
-				} else {
-					System.out.print("x ");
-				}
-				if (cont % 30 == 0) {
-					System.out.println();
-				}
-
-			}
-		}
-
-		System.out.println("posição atual: " + x + ", " + y);
-		System.out.println("direcao menor peso: " + direcaoMenorPeso);
-		System.out.println("teste acima: " + sensor.getVisaoIdentificacao()[7]);
-		System.out.println("teste abaixo: " + sensor.getVisaoIdentificacao()[16]);
-		System.out.println("teste direita: " + sensor.getVisaoIdentificacao()[12]);
-		System.out.println("teste esquerda: " + sensor.getVisaoIdentificacao()[11]);
-		System.out.println("== retorno: " + direcaoMenorPeso + " ==");
-		System.out.println("*******************************************************");
-
-		return direcaoMenorPeso;
 	}
 
-	int getDirecaoMenorPeso(int menorPeso) {
+	Objetivo decidirObjetivo(){
+		//ArrayList<Visao> visao = getMovimentosPossiveis();
 
-
-
-		return 0;
+		return Objetivo.EXPLORAR;
+	}
+	
+	boolean movimentoIsViavel(int coordenadaVisao){
+		return getResultVisao(sensor.getVisaoIdentificacao()[coordenadaVisao]) == Visao.VAZIO || getResultVisao(sensor.getVisaoIdentificacao()[coordenadaVisao]) == Visao.MOEDA || getResultVisao(sensor.getVisaoIdentificacao()[coordenadaVisao]) == Visao.PASTINHA;
 	}
 
-	boolean isCaminhoViavel(int indice){
-		return indice == 0 || indice == 4;
-	}
 
-	int getPesoMemoria(int x, int y){
-		return memoriaExploracao[x][y];
-	}
+
 }
